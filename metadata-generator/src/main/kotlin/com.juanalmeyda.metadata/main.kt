7726a633ed0b4f63e2a9cbab7ecf03cf613e97fe
Juan Almeyda <almeydajuan@gmail.com>
2024-05-30 15:40:33 +0200

    first version of library to generate yaml files

--- metadata-generator/src/main/kotlin/com.juanalmeyda.metadata/main.kt
+++ metadata-generator/src/main/kotlin/com.juanalmeyda.metadata/main.kt
@@ -1,5 +1,21 @@
 package com.juanalmeyda.metadata
 
+import java.io.File
+
 fun main() {
-    println("I am a library")
+    val content = generateFileContent("other")
+
+    val file = File("metadata-generator/config.yml")
+    if (file.exists()) {
+        file.delete()
+    }
+    file.writeText(content)
+
+    println("YML file generated successfully")
 }
+
+private fun generateFileContent(serviceName: String) = """
+        |version: 1
+        |service:
+        |  name: $serviceName
+    """.trimMargin()
