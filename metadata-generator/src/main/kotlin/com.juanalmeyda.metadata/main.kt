94882be1c66ace0a89b1d6b0bd3950c9c67245f0
Juan Almeyda <almeydajuan@gmail.com>
2024-05-30 17:19:33 +0200

    remove main logic from library

--- metadata-generator/src/main/kotlin/com.juanalmeyda.metadata/main.kt
+++ metadata-generator/src/main/kotlin/com.juanalmeyda.metadata/main.kt
@@ -1,26 +1,5 @@
 package com.juanalmeyda.metadata
 
-import com.juanalmeyda.metadata.yaml.yaml
-import java.io.File
-
-fun main(array: Array<String>) {
-    val yaml: String = runCatching {
-        array[0]
-    }.getOrElse { default() }
-
-    // TODO: parameterize
-    val file = File("metadata-generator/config.yml")
-    if (file.exists()) {
-        file.delete()
-    }
-//    file.writeText(content)
-    print(yaml)
+fun main() {
+    println("I'm a library")
 }
-
-fun default() =
-    yaml {
-        version(1)
-        service {
-            name("my-service")
-        }
-    }
