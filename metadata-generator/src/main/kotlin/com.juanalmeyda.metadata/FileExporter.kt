89705a5d960ec32a4e6a1328a4f9f77fe91d41b3
Juan Almeyda <almeydajuan@gmail.com>
2024-05-31 11:37:18 +0200

    hide yaml structure from consumers of metadata generator

--- metadata-generator/src/main/kotlin/com.juanalmeyda.metadata/FileExporter.kt
+++ metadata-generator/src/main/kotlin/com.juanalmeyda.metadata/FileExporter.kt
@@ -1,13 +1,13 @@
 package com.juanalmeyda.metadata
 
+import com.juanalmeyda.metadata.yaml.YamlBuilder
+import com.juanalmeyda.metadata.yaml.YamlConfig
 import java.io.File
 
-fun FileExporter(
-    yaml: String
-) {
+fun FileExporter(yamlConfig: YamlConfig) {
     val file = File("config.yml")
     if (file.exists()) {
         file.delete()
     }
-    file.writeText(yaml)
+    file.writeText(YamlBuilder.fromConfig(yamlConfig))
 }
--- tictactoe4k/src/main/kotlin/com/juanalmeyda/tictactoe4k/metadata/generate.kt
+++ tictactoe4k/src/main/kotlin/com/juanalmeyda/tictactoe4k/metadata/generate.kt
@@ -3,22 +3,13 @@ package com.juanalmeyda.tictactoe4k.metadata
 import com.juanalmeyda.metadata.FileExporter
 import com.juanalmeyda.metadata.yaml.Characteristic.experimental
 import com.juanalmeyda.metadata.yaml.Characteristic.test
-import com.juanalmeyda.metadata.yaml.yaml
+import com.juanalmeyda.metadata.yaml.YamlConfig
 
 fun main() {
-    FileExporter(
-        yaml {
-            version(1)
-            service {
-                name("tictactoe4k")
-            }
-            characteristics {
-                characteristic(experimental)
-                characteristic(test)
-            }
-            attributes {
-                attribute(attribute = experimental to "webapp")
-            }
-        }
-    )
+    FileExporter(YamlConfig(
+        version = 1,
+        serviceName = "tictactoe4k",
+        characteristics = listOf(experimental, test),
+        attributes = mapOf(experimental to "webapp")
+    ))
 }
