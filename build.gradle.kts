1bd77097bbd69af1cbadec04bd13854ad474d166
Juan Almeyda <almeydajuan@gmail.com>
2024-06-15 14:48:31 +0200

    move task info plugin to base folder

---
+++ build.gradle.kts
@@ -0,0 +1,3 @@
+plugins {
+    id("org.barfuin.gradle.taskinfo") version "2.2.0"
+}
--- tictactoe4k/build.gradle.kts
+++ tictactoe4k/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "2.0.0"
-    id("org.barfuin.gradle.taskinfo") version "2.2.0"
 }
 
 group = "com.juanalmeyda"
