c75fec3d43e5387cac707a13e35d32b4b3135a02
Juan Almeyda <almeydajuan@gmail.com>
2024-07-01 16:57:57 +0200

    skip unstable versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import de.fayard.refreshVersions.core.FeatureFlag.VERSIONS_CATALOG
+
 include("tictactoe4k", "metadata-generator")
 
 rootProject.name = "monorepo-poc"
@@ -5,4 +7,14 @@ rootProject.name = "monorepo-poc"
 plugins {
     // See https://jmfayard.github.io/refreshVersions
     id("de.fayard.refreshVersions") version "0.60.5"
+}
+
+refreshVersions {
+    featureFlags {
+        disable(VERSIONS_CATALOG)
+    }
+    rejectVersionIf {
+        @Suppress("UnstableApiUsage")
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
 }
