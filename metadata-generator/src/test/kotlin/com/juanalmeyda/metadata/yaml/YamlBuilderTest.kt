b5a93b520f391b2acaa54895e0aecc68a494ec52
Juan Almeyda <almeydajuan@gmail.com>
2024-07-30 18:24:20 +0200

    replace old builder with new

--- metadata-generator/src/test/kotlin/com/juanalmeyda/metadata/yaml/YamlBuilderTest.kt
+++ metadata-generator/src/test/kotlin/com/juanalmeyda/metadata/yaml/YamlBuilderTest.kt
@@ -4,11 +4,9 @@ import com.juanalmeyda.metadata.yaml.Characteristic.experimental
 import com.juanalmeyda.metadata.yaml.Characteristic.other
 import com.juanalmeyda.metadata.yaml.Characteristic.test
 import org.http4k.core.ContentType.Companion.APPLICATION_YAML
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.OK
-import org.http4k.lens.contentType
 import org.http4k.testing.Approver
 import org.http4k.testing.YamlApprovalTest
+import org.http4k.testing.assertApproved
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -16,60 +14,15 @@ import org.junit.jupiter.api.extension.ExtendWith
 class YamlBuilderTest {
 
     @Test
-    fun `simple service metadata`(approver: Approver) {
-        val yamlFile = yaml {
-            version(1)
-            service {
-                name("my-service")
-            }
-        }
-        approver.assertApproved(yamlFile.wrapInResponse())
-    }
-
-    @Test
-    fun `with characteristics`(approver: Approver) {
-        val yamlFile = yaml {
-            version(1)
-            service {
-                name("service-with-characteristics")
-            }
-            characteristics {
-                characteristic(test)
-                characteristic(other)
-                characteristic(experimental)
-            }
-        }
-        approver.assertApproved(yamlFile.wrapInResponse())
-    }
-
-    @Test
-    fun `with attributes`(approver: Approver) {
-        val yamlFile = yaml {
-            version(1)
-            service {
-                name("service-with-attributes")
-            }
-            attributes {
-                attribute(attribute = test to "first")
-                attribute(attribute = other to "maybe")
-                attribute(attribute = experimental to "always")
-            }
-        }
-        approver.assertApproved(yamlFile.wrapInResponse())
-    }
-
-    @Test
     fun `initialise yaml through config`(approver: Approver) {
-        val yamlConfig = YamlConfig(
+        val yamlConfig = YamlConfig2(
             version = 1,
-            serviceName = "my-service",
+            service = Service("my-service"),
             characteristics = listOf(test, other, experimental),
-            attributes = mapOf(test to "first", other to "something")
+            attributes = listOf(Attribute(test, "first"), Attribute(other, "something"))
         )
 
-        approver.assertApproved(YamlBuilder.fromConfig(yamlConfig).wrapInResponse())
+        approver.assertApproved(YamlParser.asFormatString(yamlConfig), contentType = APPLICATION_YAML)
     }
 
 }
-
-private fun String.wrapInResponse() = Response(OK).body(this).contentType(APPLICATION_YAML)
