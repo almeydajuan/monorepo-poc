6493f0031111ec59412634322392bbebf9097e9b
Juan Almeyda <almeydajuan@gmail.com>
2024-07-30 15:29:03 +0200

    introduce first layer of identation to yaml generation

--- metadata-generator/src/test/kotlin/com/juanalmeyda/metadata/yaml/ParserV2.kt
+++ metadata-generator/src/test/kotlin/com/juanalmeyda/metadata/yaml/ParserV2.kt
@@ -9,26 +9,37 @@ import org.http4k.format.ConfigurableJacksonYaml
 import org.http4k.format.asConfigurable
 import org.http4k.testing.Approver
 import org.http4k.testing.YamlApprovalTest
+import org.http4k.testing.assertApproved
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.http4k.testing.assertApproved
 
 @ExtendWith(YamlApprovalTest::class)
 class ParserV2 {
 
     @Test
     fun `minimum yaml`(approver: Approver) {
-        val yamlObject = YamlObject(version = 1)
+        val yamlObject = YamlObject(version = 1, service = SomeService("my-service"))
         approver.assertApproved(YamlParser.asFormatString(yamlObject), contentType = APPLICATION_YAML)
     }
 }
 
 data class YamlObject(
-    val version: Int
+    val version: Int,
+    val service: SomeService
+)
+
+data class SomeService(
+    val name: String
 )
 
 private object YamlParser : ConfigurableJacksonYaml(
     KotlinModule.Builder().build()
-        .asConfigurable(ObjectMapper(YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)))
+        .asConfigurable(
+            ObjectMapper(
+                YAMLFactory()
+                    .disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)
+                    .enable(YAMLGenerator.Feature.MINIMIZE_QUOTES)
+            )
+        )
         .done()
 )
