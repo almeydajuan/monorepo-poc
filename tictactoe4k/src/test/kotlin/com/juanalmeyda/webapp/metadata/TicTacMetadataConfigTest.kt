bd70b7a0e98cc60a4fd1e8c8db489086ed31d4b3
Juan Almeyda <almeydajuan@gmail.com>
2024-07-31 16:58:14 +0200

    introduce service name microtype

--- tictactoe4k/src/test/kotlin/com/juanalmeyda/webapp/metadata/TicTacMetadataConfigTest.kt
+++ tictactoe4k/src/test/kotlin/com/juanalmeyda/webapp/metadata/TicTacMetadataConfigTest.kt
@@ -5,12 +5,13 @@ import com.juanalmeyda.metadata.yaml.Attribute
 import com.juanalmeyda.metadata.yaml.Characteristic.experimental
 import com.juanalmeyda.metadata.yaml.Characteristic.test
 import com.juanalmeyda.metadata.yaml.Service
+import com.juanalmeyda.metadata.yaml.ServiceName
 import com.juanalmeyda.metadata.yaml.YamlConfig
 
 class TicTacMetadataConfigTest : YamlMetadataConfigTest {
 
     override val config = YamlConfig(
-        service = Service("Tic Tac Service"),
+        service = Service(ServiceName.of("Tic Tac Service")),
         characteristics = listOf(experimental),
         attributes = listOf(
             Attribute(experimental, "example service"),
