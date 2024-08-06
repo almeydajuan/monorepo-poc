3b875ec1d6044feb49d08c481c7e2d230baddef7
Juan Almeyda <almeydajuan@gmail.com>
2024-08-06 16:19:12 +0200

    metadata-generator: migrate version definition

--- metadata-generator/build.gradle.kts
+++ metadata-generator/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation("org.http4k:http4k-format-jackson-yaml")
+    implementation(Http4k.format.jacksonYaml)
 
     testImplementation(Http4k.format.jacksonYaml)
     testImplementation(Http4k.testing.approval)
