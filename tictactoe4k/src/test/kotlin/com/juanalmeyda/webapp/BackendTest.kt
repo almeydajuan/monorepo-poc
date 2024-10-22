a441cf7148f67d0990a19911cf1f9dc5bcbdb12d
Juan Almeyda <almeydajuan@gmail.com>
2024-10-22 11:34:01 +0200

    Refactor tests to use JUnit5 ParameterResolver for backend initialization

--- tictactoe4k/src/test/kotlin/com/juanalmeyda/webapp/BackendTest.kt
+++ tictactoe4k/src/test/kotlin/com/juanalmeyda/webapp/BackendTest.kt
@@ -2,26 +2,32 @@ package com.juanalmeyda.webapp
 
 import com.juanalmeyda.tictactoe4k.Game
 import com.juanalmeyda.tictactoe4k.Move
-import com.juanalmeyda.tictactoe4k.gameLens
-import com.juanalmeyda.tictactoe4k.newBackend
 import com.juanalmeyda.tictactoe4k.Player.O
 import com.juanalmeyda.tictactoe4k.Player.X
+import com.juanalmeyda.tictactoe4k.gameLens
+import com.juanalmeyda.tictactoe4k.newBackend
+import org.http4k.core.HttpHandler
 import org.http4k.core.Method.DELETE
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.ParameterContext
+import org.junit.jupiter.api.extension.ParameterResolver
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
+@DisplayName("Backend Tests using parameter resolvers")
 class BackendTest {
 
-    private val backend = newBackend(Game())
-
     @Test
-    fun `start new game`() {
+    @ExtendWith(NewGameParameterResolver::class)
+    fun `start new game`(backend: HttpHandler) {
         val response: Response = backend(Request(GET, "http://localhost:8080/game"))
 
         expectThat(response.status).isEqualTo(OK)
@@ -31,7 +37,8 @@ class BackendTest {
     }
 
     @Test
-    fun `make some moves`() {
+    @ExtendWith(NewGameParameterResolver::class)
+    fun `make some moves`(backend: HttpHandler) {
         expectThat(backend(Request(POST, "/game?x=0&y=1")).status).isEqualTo(OK)
         expectThat(backend(Request(POST, "/game?x=2&y=0")).status).isEqualTo(OK)
         expectThat(backend(Request(POST, "/game?x=1&y=1")).status).isEqualTo(OK)
@@ -51,12 +58,14 @@ class BackendTest {
     }
 
     @Test
+    @ExtendWith(FinishedGameParameterResolver::class)
     fun `player X wins`() {
         val backend = newBackend(finishedGame)
         expectThat(gameLens(backend(Request(GET, "/game"))).winner).isEqualTo(X)
     }
 
     @Test
+    @ExtendWith(FinishedGameParameterResolver::class)
     fun `restart game`() {
         val backend = newBackend(finishedGame)
         expectThat(backend(Request(DELETE, "/game")).status).isEqualTo(OK)
@@ -74,3 +83,21 @@ val finishedGame = Game()
     .makeMove(0, 0).makeMove(1, 0)
     .makeMove(0, 1).makeMove(1, 1)
     .makeMove(0, 2)
+
+class NewGameParameterResolver : ParameterResolver {
+
+    override fun supportsParameter(parameterContext: ParameterContext?, extensionContext: ExtensionContext?) =
+        parameterContext?.parameter?.type == HttpHandler::class.java
+
+    override fun resolveParameter(parameterContext: ParameterContext?, extensionContext: ExtensionContext?) =
+        newBackend(Game())
+}
+
+class FinishedGameParameterResolver : ParameterResolver {
+
+    override fun supportsParameter(parameterContext: ParameterContext?, extensionContext: ExtensionContext?) =
+        parameterContext?.parameter?.type == HttpHandler::class.java
+
+    override fun resolveParameter(parameterContext: ParameterContext?, extensionContext: ExtensionContext?) =
+        newBackend(finishedGame)
+}
