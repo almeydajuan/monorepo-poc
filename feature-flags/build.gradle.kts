c507d6ecbaf97df070137d2349e69f1ac722d2b3
Juan Almeyda <almeydajuan@gmail.com>
2025-02-05 15:55:46 +0100

    Add new backend service with Ktor

---
+++ feature-flags/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("backend")
+    id("io.ktor.plugin") version "3.0.3"
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:3.0.3")
+    implementation("io.ktor:ktor-server-netty:3.0.3")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+}
+
+application {
+    mainClass = "com.juanalmeyda.featureflags.ApplicationKt"
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
---
+++ feature-flags/src/main/kotlin/com/juanalmeyda/featureflags/Application.kt
@@ -0,0 +1,20 @@
+package com.juanalmeyda.featureflags
+
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
