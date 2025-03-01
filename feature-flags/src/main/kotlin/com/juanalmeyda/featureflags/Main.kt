ae05e35050a720195e6e11c6995d8ad0be497394
Juan Almeyda <almeydajuan@gmail.com>
2025-03-01 19:09:19 +0100

    Allow updating feature flags

--- feature-flags/src/main/kotlin/com/juanalmeyda/featureflags/Main.kt
+++ feature-flags/src/main/kotlin/com/juanalmeyda/featureflags/Main.kt
@@ -6,9 +6,11 @@ import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 
 fun main() {
@@ -16,6 +18,7 @@ fun main() {
 }
 
 fun Application.module() {
+    val repository = FeatureFlagRepository()
     install(ContentNegotiation) {
         jackson()
     }
@@ -26,9 +29,23 @@ fun Application.module() {
         // TODO: allow to enable/disable
         get("/flag/{id}") {
             val id = call.parameters["id"].orEmpty()
-            call.respond(FeatureFlag(name = id, enabled = false))
+            call.respond(repository.getFeatureFlag(id))
+        }
+        post("/flag") {
+            call.receive<FeatureFlag>().apply {
+                repository.updateFeatureFlag(name, enabled)
+                call.respond(repository.getFeatureFlag(name))
+            }
         }
     }
+}
+
+class FeatureFlagRepository {
+    private val flags = mutableMapOf<String, Boolean>()
+
+    fun getFeatureFlag(id: String): FeatureFlag = flags[id]?.let { FeatureFlag(id, it) } ?: FeatureFlag(id, false)
+
+    fun updateFeatureFlag(id: String, enabled: Boolean) = flags.put(id, enabled)
 }
 
 data class FeatureFlag(val name: String, val enabled: Boolean) {
--- feature-flags/src/test/kotlin/com/juanalmeyda/featureflags/ApplicationTest.kt
+++ feature-flags/src/test/kotlin/com/juanalmeyda/featureflags/ApplicationTest.kt
@@ -4,8 +4,12 @@ import com.juanalmeyda.featureflags.FeatureFlag.Companion.AI_OPPONENT
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.http.contentType
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.testing.testApplication
 import org.junit.jupiter.api.Test
@@ -40,4 +44,45 @@ class ApplicationTest {
         expectThat(response.status).isEqualTo(OK)
         expectThat(response.body<FeatureFlag>()).isEqualTo(FeatureFlag(name = AI_OPPONENT, enabled = false))
     }
+
+    @Test
+    fun `enable feature flag`() = testApplication {
+        application {
+            module()
+        }
+        val client = createClient {
+            install(ContentNegotiation) {
+                jackson()
+            }
+        }
+        val response = client.post("/flag") {
+            contentType(ContentType.Application.Json)
+            setBody(FeatureFlag(name = AI_OPPONENT, enabled = true))
+        }
+
+        expectThat(response.status).isEqualTo(OK)
+        expectThat(response.body<FeatureFlag>()).isEqualTo(FeatureFlag(name = AI_OPPONENT, enabled = true))
+    }
+
+    @Test
+    fun `enable and retrieve feature flag`() = testApplication {
+        application {
+            module()
+        }
+        val client = createClient {
+            install(ContentNegotiation) {
+                jackson()
+            }
+        }
+        client.post("/flag") {
+            contentType(ContentType.Application.Json)
+            setBody(FeatureFlag(name = AI_OPPONENT, enabled = true))
+        }
+
+        val response = client.get("/flag/$AI_OPPONENT")
+
+        expectThat(response.status).isEqualTo(OK)
+        expectThat(response.body<FeatureFlag>()).isEqualTo(FeatureFlag(name = AI_OPPONENT, enabled = true))
+    }
+
 }
