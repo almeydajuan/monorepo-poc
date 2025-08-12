3e4508bd40c43c1f69ed41bb8a7fd4778af5b136
Juan Almeyda <juan.almeyda@cardino.de>
2025-08-12 11:27:27 +0200

    Fix infra dependencies configuration

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -8,10 +8,8 @@ dependencies {
     implementation(platform(libs.http4k.bom))
 
     // Using version catalog for http4k modules
-    api(libs.http4k.core)
     api(libs.http4k.config)
-    api(libs.http4k.platform.core)
-    api(libs.http4k.platform.k8s)
+    implementation(libs.http4k.platform.k8s)
 
     // Using version catalog for exposed
     implementation(libs.bundles.exposed)
