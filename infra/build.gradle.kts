1bad668baf3d17c190fb141f8bf6bce56bc862fb
Juan Almeyda <almeydajuan@gmail.com>
2025-08-28 21:21:28 +0200

    Remove unused bundle

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 }
 
 dependencies {
-    // Using version catalog for BOM
     implementation(platform(libs.http4k.bom))
 
-    // Using version catalog for http4k modules
     api(libs.http4k.config)
     implementation(libs.http4k.platform.k8s)
 
-    // Using version catalog for exposed
     implementation(libs.bundles.exposed)
 
-    // Using version catalog for testing
     testApi(libs.strikt.core)
 }
